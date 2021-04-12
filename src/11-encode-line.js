2fe5dcea82364211faffe526c220da8ecebd1f72
Raman Karseka <ier@live.ru>
2021-04-13 00:32:41 +0300

    feat: task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
-}
+function encodeLine(str) {
+  const arr = str.split('');
+  console.log(arr);
+  let def = 0;
+  let res = '';
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      res += ((i - def) > 1 ? i - def : '') + arr[i - 1];
+      def = i;
+    }
+  }
 
+  return res;
+}
 module.exports = encodeLine;
